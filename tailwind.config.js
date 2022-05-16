02595c74ccee2f35d5ec6a774b3746cc1c547c9a
BagusSedana <gedebagussedanayoga28@gmail.com>
2022-05-16 15:36:40 +0800

    Add Tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
     content: ['index.html'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#0ea5e9',
+                dark: '#0f172a',
+                dark_1: '#64748b',
+                dark_2: '#94a3b8',
+            }
+        },
     },
     plugins: [],
 }
